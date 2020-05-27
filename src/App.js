c745e432687c3a8dbf52e2dc179616a1d774dc6f
Alan Li <al2hkm@virginia.edu>
2020-05-27 11:07:08 -0400

    fix env

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import axios from 'axios';
 import Restaurants from './Components/Restaurants.js';
 import InputBar from './Components/InputBar.js';
+import './.env'
 
 class App extends React.Component {
   constructor(props) {
@@ -21,18 +22,28 @@ class App extends React.Component {
 
 
   //should run this when enter is clicked in inputbar
-  //pass in a coords object with lat and lng fields
-  //locationbias = circle: 2000@47.6918452,-122.2226413
-  //use lat lng to do location bias
+  //pass in lat and lng fields
   //radius is in meters
-  getList(radius, lat, lng) {
+  //location is the address but in lat lng
+  //can enter address into keyword too if no need for search food functionality
+  getList(radi, lat, lng, category, query) {
+    const linkHead = 'https://maps.googleapis.com/maps/api/place/textsearch/json?'
+    const location = 'location=' + lat.toString() + ',' + lng.toString()
+    const radius = '&radius=' + radi.toString()
+    const type = '&type=' + category.toString()
+    const keyword = '&keyword=' + query.toString()
+    // const rankby = '&rankby=distance'
+    const key = '&opennow&key=' + process.env
+    const url = linkHead + location + radius + type + keyword + key;
     axios
       .get(
-        'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=roots%charlottesville&inputtype=textquery&fields=name,formatted_address,geometry,opening_hours,rating,price_level,photos&locationbias=circle:{radius}@{lat},{lng}&key=AIzaSyBQRbOl8Z5HnrY12zURP84C6Tdwsoy-HUI'
+        url
       )
       .then(response => {
-        const responseList = response.data.candidates
-        for (let i = 0; i < responseList.length; i++) {
+        const responseList = response.data.results
+        console.log(responseList[0])
+        //can make i results.length if want all of them
+        for (let i = 0; i < 10; i++) {
           const place = responseList[i];
           const restaurant = {
             name: place.name,
@@ -40,7 +51,7 @@ class App extends React.Component {
             //location stored as a pair of coords: "lat" and "lng"
             coords: place.geometry.location,
             hours: place.opening_hours,
-            rating: place.candidates,
+            rating: place.rating,
             // reviews: place.reviews, will need to add to url too
             price: place.price_level,
             // phone: place.formatted_phone_number,
@@ -52,35 +63,15 @@ class App extends React.Component {
           //   restaurantList: [...prevState.restaurantList, restaurant]
           // }))
         }
-        // responseList.map(place => {
-        //   const restaurant = {
-        //     name: place.name,
-        //     address: place.formatted_address,
-        //     //location stored as a pair of coords: "lat" and "lng"
-        //     coords: place.geometry.location,
-        //     hours: place.opening_hours,
-        //     rating: place.candidates,
-        //     // reviews: place.reviews, will need to add to url too
-        //     price: place.price_level,
-        //     // phone: place.formatted_phone_number,
-        //     // website: place.website,
-        //     photos: place.photos,
-        //   }
-        //   console.log(restaurant)
-        //   // this.setState(prevState => ({
-        //   //   restaurantList: [...prevState.restaurantList, restaurant]
-        //   // }))
-        // })
       })
-      .catch(error => {
-        console.log('error')
-      })
+    // .catch(error => {
+    //   console.log('error')
+    // })
   }
 
 
-
   render() {
-    this.getList(2000, 38.0293, 78.4767);
+    this.getList(2000, 38.0293, -78.4767, 'restaurant', 'food');
     return (
       <div>
         <div>hi</div>
