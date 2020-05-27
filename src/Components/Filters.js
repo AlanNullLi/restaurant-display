8971fd2ed2a7e4e02f60551bea4fd0c4d93c2dfc
Grace <gb4yy@virginia.edu>
2020-05-27 12:40:21 -0400

    deleted unused import and added default to switch

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -4,124 +4,124 @@ import './Filter.css'
 
 
 class Filters extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            longitude: null,
-            latitude: null,
-            userAddress: null
-        }
-
-        this.getLocation = this.getLocation.bind(this)
-        this.getCoordinates = this.getCoordinates.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      longitude: null,
+      latitude: null,
+      userAddress: null
     }
 
-    getLocation = () => {
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition(this.getCoordinates, this.handleLocationError);
-          } else {
-            alert("Geolocation is not supported by this browser.");
-          }
+    this.getLocation = this.getLocation.bind(this)
+    this.getCoordinates = this.getCoordinates.bind(this)
+  }
+
+  getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.getCoordinates, this.handleLocationError);
+    } else {
+      alert("Geolocation is not supported by this browser.");
     }
+  }
 
-    getCoordinates = (position, ) => {
-        console.log("hi");
-        console.log(position);
-        this.setState({
-            latitude: position.coords.latitude,
-            longitude: position.coords.longitude
-        })
+  getCoordinates = (position, ) => {
+    console.log("hi");
+    console.log(position);
+    this.setState({
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    })
+  }
+
+  handleLocationError = (error) => {
+    switch (error.code) {
+      case error.PERMISSION_DENIED:
+        alert("User denied the request for Geolocation.");
+        break;
+      case error.POSITION_UNAVAILABLE:
+        alert("Location information is unavailable.");
+        break;
+      case error.TIMEOUT:
+        alert("The request to get user location timed out.");
+        break;
+      default:
+        alert("An unknown error occurred.");
+        break;
     }
+  }
 
-    handleLocationError = (error) => {
-        switch(error.code) {
-            case error.PERMISSION_DENIED:
-              alert("User denied the request for Geolocation.");
-              break;
-            case error.POSITION_UNAVAILABLE:
-              alert("Location information is unavailable.");
-              break;
-            case error.TIMEOUT:
-              alert("The request to get user location timed out.");
-              break;
-            case error.UNKNOWN_ERROR:
-              alert("An unknown error occurred.");
-              break;
-          }
-    }
+  render() {
 
-    render(){
+    const rating = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+            ★
+                </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+            ★★
+                </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+            ★★★
+                </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+            ★★★★
+                </a>
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+            ★★★★★
+                </a>
+        </Menu.Item>
+      </Menu>
+    );
 
-        const rating = (
-            <Menu>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-                ★
+    const price = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+            $
                 </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
-                ★★
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
+            $$
                 </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-                ★★★
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+            $$$
                 </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-                ★★★★
+        </Menu.Item>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
+            $$$$
                 </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-                ★★★★★
-                </a>
-              </Menu.Item>
-            </Menu>
-          );
+        </Menu.Item>
+      </Menu>
+    );
 
-          const price = (
-            <Menu>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-                $
-                </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.taobao.com/">
-                $$
-                </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-                $$$
-                </a>
-              </Menu.Item>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.tmall.com/">
-                $$$$
-                </a>
-              </Menu.Item>
-            </Menu>
-          );
 
- 
-        return(
-            <div classname='FiltersClass'>
-                    <Dropdown overlay={rating} placement="bottomLeft">
-                        <Button>Rating</Button>
-                    </Dropdown>
+    return (
+      <div classname='FiltersClass'>
+        <Dropdown overlay={rating} placement="bottomLeft">
+          <Button>Rating</Button>
+        </Dropdown>
 
-                    <Dropdown overlay={price} placement="bottomLeft">
-                        <Button>Pricing</Button>
-                    </Dropdown>
-                    <button onClick={this.getLocation}>Get Location</button>
-                    {this.state.latitude} {this.state.longitude}
-            </div>
-        )
-    }
+        <Dropdown overlay={price} placement="bottomLeft">
+          <Button>Pricing</Button>
+        </Dropdown>
+        <button onClick={this.getLocation}>Get Location</button>
+        {this.state.latitude} {this.state.longitude}
+      </div>
+    )
+  }
 }
 
 export default Filters;
--- src/Components/Restaurants.js
+++ src/Components/Restaurants.js
@@ -4,7 +4,6 @@ import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
 import { CardHeader } from '@material-ui/core';
 import Rating from '@material-ui/lab/Rating';
 import { withStyles } from '@material-ui/core/styles';
