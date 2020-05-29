5eaf34f1a868dd56e584eaceafd08fd6c2b26b08
Grace <gb4yy@virginia.edu>
2020-05-29 10:20:58 -0400

    fixed merge problems in filters.js

--- src/Components/Filters.js
+++ src/Components/Filters.js
@@ -1,28 +1,23 @@
 import React from 'react';
-import { Menu, Dropdown, Button } from 'antd';
+import { Menu, Dropdown, Button, Input } from 'antd';
 import './Filter.css'
 // import { } from '../App.js'
 
 
 class Filters extends React.Component {
-    constructor(props){
-        super(props);
-        this.state = {
-            longitude: null,
-            latitude: null,
-            userAddress: null
-        }
-
-        this.getLocation = this.getLocation.bind(this)
-        this.getCoordinates = this.getCoordinates.bind(this)
-        this.getUserAddress = this.getUserAddress.bind(this)
-
+  constructor(props) {
+    super(props);
+    this.state = {
+      longitude: null,
+      latitude: null,
+      userAddress: null
     }
 
     this.getLocation = this.getLocation.bind(this)
     this.getCoordinates = this.getCoordinates.bind(this)
-  }
+    this.getUserAddress = this.getUserAddress.bind(this)
 
+  }
 
   getLocation = () => {
     if (navigator.geolocation) {
@@ -58,19 +53,19 @@ class Filters extends React.Component {
     }
   }
 
-    getUserAddress = (e) => {
-      this.setState({
-        userAddress: e.target.value
-      })
-      // this.updateRestaurants.props(this.props.userAddress)
-    }
+  getUserAddress = (e) => {
+    this.setState({
+      userAddress: e.target.value
+    })
+    // this.updateRestaurants.props(this.props.userAddress)
+  }
 
-    render(){
-        const rating = (
-            <Menu>
-              <Menu.Item>
-                <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
-                ★
+  render() {
+    const rating = (
+      <Menu>
+        <Menu.Item>
+          <a target="_blank" rel="noopener noreferrer" href="http://www.alipay.com/">
+            ★
 
                 </a>
         </Menu.Item>
@@ -122,29 +117,33 @@ class Filters extends React.Component {
       </Menu>
     );
 
- 
-        return(
-            <div className='FiltersClass'>
-                    <Dropdown overlay={rating} placement="bottomLeft">
-                        <Button>Rating</Button>
-                    </Dropdown>
+    return (
+      <div className='FiltersClass'>
+        <Dropdown overlay={rating} placement="bottomLeft">
+          <Button>Rating</Button>
+        </Dropdown>
 
-                    <Dropdown overlay={price} placement="bottomLeft">
-                        <Button>Pricing</Button>
-                    </Dropdown>
+        <Dropdown overlay={price} placement="bottomLeft">
+          <Button>Pricing</Button>
+        </Dropdown>
 
-                    <input type="text" id="textArea" placeholder="Input Address" value={this.state.userAddress}  onChange={e=>this.getUserAddress(e)}> 
-                    </input>      
-
-                    {/* <button type='submit' onClick={this.getUserAddress}>Submit Location</button> */}
-                    <button type='submit' onClick={() => this.props.updateRestaurants(this.state.userAddress)}>Submit Location</button>
-
-                    <button onClick={this.getLocation}>Share Location</button>
-                    {this.state.latitude} {this.state.longitude}
-            </div>
-        )
-    }
-
+        <Input
+          id='textArea'
+          placeholder='Input Address'
+          value={this.state.userAddress}
+          onChange={(e) => this.getUserAddress(e)}
+        ></Input>
+        <Button
+          type='submit'
+          onClick={() => this.props.updateRestaurants(this.state.userAddress, 38.0336, -78.5080, 5000, 4)}
+        >Submit Address</Button>
+        <Button
+          onClick={this.getLocation}
+        >Share Location</Button>
+      </div >
+    )
+  
+}
 }
 
 export default Filters;
